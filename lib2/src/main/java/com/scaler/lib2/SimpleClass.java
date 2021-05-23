b845b74a0feae7511026566c1ea501d677c37cd0
Nirmit Maheshwari <24971496+mfsi-nirmitm@users.noreply.github.com>
2021-05-23 19:32:19 +0530

    added children module
    added children module

---
+++ lib2/src/main/java/com/scaler/lib2/SimpleClass.java
@@ -0,0 +1,10 @@
+package com.scaler.lib2;
+
+public class SimpleClass {
+    public int a = 10;
+    String b = "something";
+
+    public void incr() {
+        a++;
+    }
+}
--- src/main/java/com/scaler/gradleproject/Main.java
+++ src/main/java/com/scaler/gradleproject/Main.java
@@ -1,6 +1,6 @@
 package com.scaler.gradleproject;
 
-
+import com.scaler.lib2.SimpleClass;
 import com.github.slugify.Slugify;
 
 public class Main {
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("Hello World");
         Slugify slg = new Slugify();
         System.out.println(slg.slugify("Hello World"));
+
+        SimpleClass sampleObj = new SimpleClass();
+        sampleObj.incr();
+        System.out.println(sampleObj.a);
     }
 }
